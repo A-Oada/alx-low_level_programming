cc9ade61b11885aec8e6529a4655722220429440
A-oada <abdulrahmanoada@gmail.com>
2023-02-17 10:15:18 -0800

    Updated 6-size.c to fix style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,8 @@
  * li: long int size
  * lli: long long int size
  * f: float size
+ *
+ * Return: Always 0
  */
 int main(void)
 {
@@ -21,6 +23,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	
+
 	return (0);
 }

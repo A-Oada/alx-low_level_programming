188c608cbb9b51f516e6f7924b334fe87be6bd53
A-oada <abdulrahmanoada@gmail.com>
2023-02-17 09:25:43 -0800

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * puts.c - prints the phrase "Programming is like building a multilingual puzzle"
+ *
+ * main - entry point
+ * 
+ * Return: Always 0
+ */
+
+int main (void)
+{
+	char phrase[] = "\"Programming is like building a multilingual puzzle\n";
+	
+	puts(phrase);
+
+	return 0;
+}

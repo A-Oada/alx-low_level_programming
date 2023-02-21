25897f4c728914074663aeb358b76d40fd4cfbb1
A-oada <abdulrahmanoada@gmail.com>
2023-02-21 09:46:30 -0800

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+# include "main.h"
+
+/**
+ * print_alphabet - Prints alphabet in lowercase
+ * 
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}

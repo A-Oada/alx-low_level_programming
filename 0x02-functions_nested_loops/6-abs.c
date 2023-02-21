be5e96b059b1d791a87ca5c43642a7677285e6b5
A-oada <abdulrahmanoada@gmail.com>
2023-02-21 10:42:44 -0800

    Added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+# include "main.h"
+
+/**
+ * _abs - Calculates the absolute value of a number
+ * @j: Number passed to function
+ *
+ * Return: Absolute value of input argument
+ */
+int _abs(int j)
+{
+	if (j < 0)
+	{
+		j = -1 * j;
+	}
+
+	return (j);
+}

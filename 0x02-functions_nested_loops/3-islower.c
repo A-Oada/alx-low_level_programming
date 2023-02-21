35287101823918a77898d15968a5ef497ef70a7e
A-oada <abdulrahmanoada@gmail.com>
2023-02-21 10:06:33 -0800

    Added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+# include "main.h"
+
+/**
+ * _islower - checks whether character is lowercase
+ * @c: Charcater passed by user to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

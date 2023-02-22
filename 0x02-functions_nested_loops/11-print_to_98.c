e6ba9f58d0ed26c95b1314f6d69bd9bfd589c5bf
A-oada <abdulrahmanoada@gmail.com>
2023-02-22 10:32:39 -0800

    Updated 11-print_to_98.c to print all integers to 98 including -ve values

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,33 @@
  */
 void print_to_98(int i)
 {
-	while (i <= 98)
+	if (i <= 98)
+		while (i <= 98)
+		{
+			if (i < 98)
+			{
+				printf("%i, ", i);
+			}
+			else
+			{
+				printf("%i", i);
+			}
+			i++;
+		}
+	else
 	{
-		if (i < 98)
+		while (i >= 98)
 		{
-			printf("%i, ", i);
+			if (i > 98)
+			{
+				printf("%i, ", i);
+			}
+			else
+			{
+				printf("%i", i);
+			}
+			i--;
 		}
-		else
-		{
-			printf("%i", i);
-		}
-		i++;
 	}
 	printf("\n");
 }

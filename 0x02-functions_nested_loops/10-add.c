77af52c96f3ac5a9943f7f3b4d7b3ab5a2a8b2d2
A-oada <abdulrahmanoada@gmail.com>
2023-02-22 10:00:02 -0800

    Added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+# include "main.h"
+
+/**
+ * add - Adds to integers
+ * @i: First number
+ * @j: Second number
+ *
+ * Return: Sum of the two digits
+ */
+int add(int j, int i)
+{
+	int k = j + i;
+
+	return (k);
+}

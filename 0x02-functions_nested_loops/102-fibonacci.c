8c7ac32fdfa6176c5323a24701ac94c8dbc9beee
A-oada <abdulrahmanoada@gmail.com>
2023-02-23 02:52:55 -0800

    Updated 102-fibonacci.c to accomodate for C89

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,29 +1,33 @@
 # include <stdio.h>
 
 /**
- * main - Entry point - Prints first 50 fibonacci sequence numbers
- *
- * Return: Always 0
- */
-int main(void)
+  * main - Entry point - Prints first 50 fibonacci sequence numbers
+  *
+  * Return: Always 0
+  */
+ int main(void)
 {
-	int fib[49] = {1, 2}, i = 2, j = 0;
+        unsigned long int fib[49] = {1, 2};
+        int i = 2, j = 0;
+        
+        /* Store the values of the sequence in an array*/
+        while (i <= 47) /* The first two values are already stored in the sequence*/
+        {
+                fib[i] = fib[i - 1] + fib[i - 2];
+                i++;
+        }
+        j = 0;
+         /* Print the elements of the loop*/
+         while (j <= 47)
+        {
+                printf("%lu", fib[j]);
+                if (j != 47)
+                {
+                        printf(", ");
+                }
+                j++;
+        }
+        printf("\n");
 
-	/* Store the values of the sequence in an array*/
-	while (i < 50)
-	{
-		fib[i] = fib[i - 1] + fib[i - 2];
-		i++;
-	}
-	/* Print the elements of the loop*/
-	while (j < 49)
-	{
-		printf("%i", fib[j]);
-		if (j != 49)
-		{
-			printf(", ");
-		}
-		j++;
-	}
+        return (0);
 }
-
